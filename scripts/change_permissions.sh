240acab53e1ce012fd697eff6b08faa4f31a07b4
TAKAHASHI Yuki <y-takahashi@begood-tech.com>
2016-03-26 08:24:29 +0900

    first commit codedeploy appspec

---
+++ scripts/change_permissions.sh
@@ -0,0 +1,1 @@
+#!/bin/bashchown -R apache:apache /var/www/html/*chmod -R 744 /var/www/html/*
---
+++ scripts/stop_server.sh
@@ -0,0 +1,1 @@
+#!/bin/bashisExistApp=`pgrep httpd`if [[ -n  \$isExistApp ]]; then   service httpd stopfi
